a6e7a96b5a8f9d1b9faaf147a068c9f277032cfb
Red <red@infect.me>
2015-05-11 19:28:36 +0000

    Made DNS resolution test parallel.

--- apps/sslshadow/lib/sslshadow/recv/test.ex
+++ apps/sslshadow/lib/sslshadow/recv/test.ex
@@ -15,13 +15,18 @@ defmodule Sslshadow.Recv.Test do
     File.read!(Application.get_env(:testenv, :dir))
     |> String.split("\n")
     |> Enum.map(&to_char_list/1)
-    |> Enum.filter(&validFQDN/1)
-    |> Enum.map(&(:inet_res.getbyname(&1, :a)))
-    |> Enum.filter(&validResponse?/1)
-    |> Enum.map(fn({:ok, {_,_,_,_,_,list}}) -> list end )
-    |> List.flatten
-    |> Enum.map(&tupleToCharIP/1)
-    |> Enum.map(fn(ipchar) -> inject({ipchar, 443}) end)
+    |> Enum.filter(&validFQDN?/1)
+    |> Enum.map(&(Process.spawn(Sslshadow.Recv.Test, :spawnHostname, [&1], [])))
+#    |> Enum.map(fn(hostname) -> Process.spawn(Sslshadow.Recv.Test, &spawnHostname/1, [hostname], []) end )
+
+
+
+#    |> Enum.map(&(:inet_res.getbyname(&1, :a)))
+#    |> Enum.filter(&validResponse?/1)
+#    |> Enum.map(fn({:ok, {_,_,_,_,_,list}}) -> list end )
+#    |> List.flatten
+#    |> Enum.map(&tupleToCharIP/1)
+#    |> Enum.map(fn(ipchar) -> inject({ipchar, 443}) end)
 
   end
 
@@ -32,7 +37,15 @@ defmodule Sslshadow.Recv.Test do
     d = Integer.to_string(d)
     Enum.join([a,b,c,d], ".")
     |> to_char_list
+  end
 
+  def spawnHostname(charlist) do
+    case validResponse?(:inet_res.getbyname(charlist, :a)) do
+              nil -> Logger.debug("No A records for " <> to_string(charlist))
+       {:hostent, _,_,_,_, data} -> #IO.inspect data
+                                    Enum.map(data, &tupleToCharIP/1)
+                                    |> Enum.map(fn(ipchar) -> inject({ipchar, 443}) end )
+    end
   end
 
 
@@ -47,10 +60,10 @@ defmodule Sslshadow.Recv.Test do
   end
 
 
-  defp validFQDN([]) do
+  defp validFQDN?([]) do
     nil
   end
-  defp validFQDN(charlist) do
+  defp validFQDN?(charlist) do
     charlist
   end
 
