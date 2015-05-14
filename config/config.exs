2013c517c2412efc6c5886315d931b4adc10f1d2
Red <red@infect.me>
2015-05-14 08:03:36 +0000

    Update configuration

--- config/config.exs
+++ config/config.exs
@@ -8,11 +8,6 @@ config :sslshadow, ssltimeout:  3000,   # Number of ms to connect to remote serv
                    poolsize: 100,        # Number of "pre-forked" ssl server processes
                    poolover: 5      # Number of extra pool members to be spawned before
                                          # we start queuing
-config :mnesia,     dir: '/home/red/projects/sslwatch/mnesia-data', # Yes, must be single quotes
+config :mnesia,     dir: '/home/sslwatch/mnesia-data', # Yes, must be single quotes
                     dc_dump_limit:  100,
                     dump_log_write_threshold: 100000 
-#config :testenv,    dir: '/home/red/projects/sslwatch/apps/sslshadow/test/testdata' # Yes, MUST be single quotes
-config :testenv,    dir: '/home/red/projects/sslwatch/apps/sslshadow/test/testdata-orig' # Yes, MUST be single quotes
-config  :sasl,    error_logger_mf_dir: '/home/red/projects/sslwatch/sasl-data',
-                  error_logger_mf_maxbytes: 10000,
-                  error_logger_mf_maxfiles: 100
